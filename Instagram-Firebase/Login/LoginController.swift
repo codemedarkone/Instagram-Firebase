fe46098fc897fba695bc0904db823dd0c3f0c2f0
Chris <codemedarkone@gmail.com>
2018-08-13 17:51:24 -0600

    added Instagram logo

--- Instagram-Firebase/Login/LoginController.swift
+++ Instagram-Firebase/Login/LoginController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class LoginController: UIViewController {
     
+    let logoContainerView: UIView = {
+        let view = UIView()
+        
+        let logoImageView = UIImageView(image: #imageLiteral(resourceName: "Instagram_logo_white"))
+        logoImageView.contentMode = .scaleAspectFill
+        
+        view.addSubview(logoImageView)
+        logoImageView.anchor(top: nil, left: nil, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 200, height: 50)
+        logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        view.backgroundColor = UIColor.rgb(red: 0, green: 120, blue: 175)
+        return view
+    }()
+    
     let signUpButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Dont have an account? Sign Up.", for: .normal)
@@ -26,10 +41,17 @@ class LoginController: UIViewController {
         
     }
     
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(logoContainerView)
+        logoContainerView.anchor(top: view.topAnchor, left: view.leadingAnchor, bottom: nil, right: view.trailingAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 150)
         
         navigationController?.isNavigationBarHidden = true
         
