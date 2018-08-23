af5590899b1d7abca4ab9d9c879d26e27f5de84d
Chris <codemedarkone@gmail.com>
2018-08-23 16:34:29 -0600

    added a searcher and  custom cell for searching users and added collection view for the searcher

---
+++ Instagram-Firebase/Search/UserSearchCell.swift
@@ -0,0 +1,50 @@
+//
+//  UserSearchCell.swift
+//  Instagram-Firebase
+//
+//  Created by chris  on 8/23/18.
+//  Copyright © 2018 kuronuma studios. All rights reserved.
+//
+
+import UIKit
+
+class UserSearchCell: UICollectionViewCell {
+    
+    let profileImageView: CustomImageView = {
+        let iv = CustomImageView()
+        iv.backgroundColor = .red
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Username"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(profileImageView)
+        addSubview(usernameLabel)
+        
+        profileImageView.anchor(top: nil, left: leadingAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 50, height: 50)
+        profileImageView.layer.cornerRadius =  50 / 2
+        profileImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        usernameLabel.anchor(top: topAnchor, left: profileImageView.trailingAnchor, bottom: bottomAnchor, right: trailingAnchor, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        let seperatorView = UIView()
+        seperatorView.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        addSubview(seperatorView)
+        
+        seperatorView.anchor(top: nil, left: usernameLabel.leadingAnchor, bottom: bottomAnchor, right: trailingAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0.5)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Instagram-Firebase/Search/UserSearchController.swift
@@ -0,0 +1,57 @@
+//
+//  UserSearchController.swift
+//  Instagram-Firebase
+//
+//  Created by chris  on 8/23/18.
+//  Copyright © 2018 kuronuma studios. All rights reserved.
+//
+
+import UIKit
+
+class UserSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    let searchBar: UISearchBar = {
+        let sb = UISearchBar()
+        sb.placeholder = "Username"
+        sb.barTintColor = .gray
+        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).backgroundColor = UIColor.rgb(red: 230, green: 230, blue: 230)
+        return sb
+    }()
+    
+    let cellId = "cellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = .white
+        
+        navigationController?.navigationBar.addSubview(searchBar)
+        
+        let navBar = navigationController?.navigationBar
+        
+        searchBar.anchor(top: navBar?.topAnchor, left: navBar?.leadingAnchor, bottom: navBar?.bottomAnchor, right: navBar?.trailingAnchor, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 8, width: 0, height: 0)
+        
+        collectionView?.register(UserSearchCell.self, forCellWithReuseIdentifier: cellId)
+        
+        collectionView?.alwaysBounceVertical = true
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell =
+            collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! UserSearchCell
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: view.frame.width, height: 66)
+    }
+    
+    
+    
+}
